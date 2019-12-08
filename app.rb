b41dc7458ba097a3423c249c172905ac55320e4f
SafwanAlqulayti <57276846+SafwanAlqulayti@users.noreply.github.com>
2019-12-08 19:41:43 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,1 +1,29 @@
 
+  i = 1
+  while i <= 100
+  result = " "
+    # Your code goes in here.
+    if i % 3 == 0  && i % 5 == 0
+   
+   result += "Fizz Buzz"
+    
+      puts result
+     i +=1
+    elsif i % 5 == 0
+ 
+    result += "Buzz"
+    puts result
+  
+    i +=1
+     elsif  i % 3 == 0
+     result += "Fizz"
+         puts result
+
+     i +=1
+    else
+     p i
+     i +=1
+    end
+   
+ 
+  end
